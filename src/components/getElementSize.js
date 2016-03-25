dd1f4618b799bc033781927d584c916eb5c3af87
WilliamLin <ganlanyuan@gmail.com>
2016-03-25 08:35:00 -0400

    update getOuterWidth/Height with a RegExp

--- src/components/getElementSize.js
+++ src/components/getElementSize.js
@@ -1,20 +1,22 @@
 /* get elements size */
 // 1. outer size: content + padding + border + margin //
 function getOuterWidth(el) {
-  var width = el.offsetWidth,
+  var pattern = /\d/, // check if value contains digital number
+      width = el.offsetWidth,
       style = el.currentStyle || getComputedStyle(el),
-      marginLeft = (style.marginLeft === 'auto') ? '0px' : style.marginLeft,
-      marginRight = (style.marginRight === 'auto') ? '0px' : style.marginRight;
+      marginLeft = (pattern.exec(style.marginLeft) === null) ? '0px' : style.marginLeft,
+      marginRight = (pattern.exec(style.marginRight) === null) ? '0px' : style.marginRight;
 
   width += parseInt(Length.toPx(el, marginLeft)) + parseInt(Length.toPx(el, marginRight));
   return width;
 }
 
 function getOuterHeight(el) {
-  var height = el.offsetHeight,
+  var pattern = /\d/, // check if value contains digital number
+      height = el.offsetHeight,
       style = el.currentStyle || getComputedStyle(el),
-      marginTop = (style.marginTop === 'auto') ? '0px' : style.marginTop,
-      marginBottom = (style.marginBottom === 'auto') ? '0px' : style.marginBottom;
+      marginTop = (pattern.exec(style.marginTop) === null) ? '0px' : style.marginTop,
+      marginBottom = (pattern.exec(style.marginBottom) === null) ? '0px' : style.marginBottom;
 
   height += parseInt(Length.toPx(el, marginTop)) + parseInt(Length.toPx(el, marginBottom));
   return height;
