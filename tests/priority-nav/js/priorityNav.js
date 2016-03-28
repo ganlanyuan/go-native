b8ac1e262de64905e9852df4a9fbfe8df0a6891d
William <ganlanyuan@gmail.com>
2016-03-28 19:25:53 -0400

    update priority-nav scss

--- tests/priority-nav/js/priorityNav.js
+++ tests/priority-nav/js/priorityNav.js
@@ -31,6 +31,7 @@ function priorityNav (navClass, buttonText, showAll, hideAll) {
     var visibleContainer = nav.querySelector('.visible-links'),
         visibleItems = visibleContainer.children,
         hiddenContainer = nav.querySelector('.hidden-links'),
+        hiddenItems = hiddenContainer.children,
         btn = nav.querySelector('.js-nav-toggle');
 
     // get breakpoints
@@ -45,6 +46,20 @@ function priorityNav (navClass, buttonText, showAll, hideAll) {
       bpV.push(newWidth);
     }
 
+    function hideItems() {
+      prepend(hiddenContainer, visibleItems[bpV.length - 1]);
+
+      bpH.unshift(bpV[bpV.length - 1]);
+      bpV.splice(-1, 1);
+    }
+
+    function showItems() {
+      append(visibleContainer, hiddenItems[0]);
+
+      bpV.push(bpH[0]);
+      bpH.shift();
+    }
+
     // update nav
     function updateNav () {
       var 
@@ -52,10 +67,9 @@ function priorityNav (navClass, buttonText, showAll, hideAll) {
           btnWidth = getOuterWidth(btn),
           outerWidth = getOuterWidth(nav),
           availableSpace = outerWidth - btnWidth,
-          target,
-          current;
+          count,
+          currentCount;
 
-      // get target, show/hide btn
       if (outerWidth >= bp[bp.length - 1] || windowWidth < showAll) {
         availableSpace = outerWidth;
         if (!btn.classList.contains('is-hidden')) {
@@ -64,11 +78,7 @@ function priorityNav (navClass, buttonText, showAll, hideAll) {
 
         if (bpV.length === bp.length) { return; }
         while(bpH.length > 0) {
-          var hiddenItems = nav.querySelectorAll('.hidden-links > li');
-          append(visibleContainer, hiddenItems[0]);
-
-          bpV.push(bpH[0]);
-          bpH.shift();
+          showItems();
         }
       } else if (windowWidth < hideAll) {
         if (btn.classList.contains('is-hidden')) {
@@ -77,10 +87,7 @@ function priorityNav (navClass, buttonText, showAll, hideAll) {
 
         if (bpH.length === bp.length) { return; }
         while(bpV.length > 0) {
-          prepend(hiddenContainer, visibleItems[bpV.length - 1]);
-
-          bpH.unshift(bpV[bpV.length - 1]);
-          bpV.splice(-1, 1);
+          hideItems();
         }
       } else {
         if (btn.classList.contains('is-hidden')) {
@@ -89,61 +96,23 @@ function priorityNav (navClass, buttonText, showAll, hideAll) {
 
         if (availableSpace <= bpV[bpV.length - 1]) {
           while(availableSpace <= bpV[bpV.length - 1]) {
-            prepend(hiddenContainer, visibleItems[bpV.length - 1]);
-
-            bpH.unshift(bpV[bpV.length - 1]);
-            bpV.splice(-1, 1);
+            hideItems();
           }
         } else {
           while(availableSpace > bpH[0]) {
-            var hiddenItems = nav.querySelectorAll('.hidden-links > li');
-            append(visibleContainer, hiddenItems[0]);
-
-            bpV.push(bpH[0]);
-            bpH.shift();
+            hiddenItems = hiddenContainer.children;
+            showItems();
           }
         }
       }
 
-      // // set current
-      // var visibleItems = nav.querySelectorAll('.visible-links > li');
-      // current = (visibleItems.length) ? visibleItems.length : 0;
-      // // if (visibleItems.length) {
-      // // } else {
-      // //   current = 0;
-      // // }
-
-      // // update
-      // if (target > current) {
-      //   var a = current;
-      //   while (a < target) {
-      //     var vlink = nav.querySelectorAll('.visible-links');
-      //     var hlinks = nav.querySelectorAll('.hidden-links > li');
-
-      //     append(vlink, hlinks[0]);
-      //     a++;
-      //   }
-      // } else if (target < current) {
-      //   var a = current;
-      //   while (a > target ) {
-      //     var visibleItems = nav.querySelectorAll('.visible-links > li');
-      //     var hlinks = nav.querySelectorAll('.hidden-links > li');
-
-      //     prepend(hiddenContainer, visibleItems[visibleItems.length - 1]);
-      //     a--;
-      //   }
-      // }
-
       // update data-count
-      var hiddenItems = nav.querySelectorAll('.hidden-links > li'),
-          count;
-
-      if (hiddenItems.length) {
-        count = hiddenItems.length;
-      } else {
-        count = 0;
+      currentCount = currentCount || 0;
+      count = bpH.length || 0;
+      if (count !== currentCount) {
+        currentCount = btn.getAttribute("data-count");
+        btn.setAttribute("data-count", count);
       }
-      btn.setAttribute("data-count", count);
     }
 
     // run updateNav
@@ -154,7 +123,7 @@ function priorityNav (navClass, buttonText, showAll, hideAll) {
 
     // show / hide hidden-links
     btn.addEventListener('click', function () {
-      nav.querySelector('.hidden-links').classList.toggle('is-hidden');
+      hiddenContainer.classList.toggle('is-hidden');
     });
   // } else {
   //   console.log('"' + navClass + '" can\'t be found.');
