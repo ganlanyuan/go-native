632e75b5b0c46c276c89c51fc284b4a9c896d037
William <ganlanyuan@gmail.com>
2016-03-30 12:08:53 -0400

    fix append, prepend, now they accept NodeList parameter

--- src/components/append.js
+++ src/components/append.js
@@ -2,13 +2,21 @@
 var append = function(els, data) {
   var els_new = (isNodeList(els)) ? els : [els];
 
-  if (typeof data.nodeType !== "undefined" && data.nodeType === 1 || isNodeList(data)) {
+  if (typeof data.nodeType !== "undefined" && data.nodeType === 1) {
     for (var i = els_new.length; i--;) {
       els_new[i].appendChild(data);
     }
   } else if (typeof data === "string") {
     for (var i = els_new.length; i--;) {
       els_new[i].insertAdjacentHTML('beforeend', data);
+    }
+  } else if (isNodeList(data)) {
+    var fragment = document.createDocumentFragment();
+    for (var i = data.length; i--;) {
+      fragment.insertBefore(data[i], fragment.firstChild);
+    }
+    for (var j = els_new.length; j--;) {
+      els_new[j].appendChild(fragment);
     }
   }
 };
--- src/components/prepend.js
+++ src/components/prepend.js
@@ -2,13 +2,21 @@
 var prepend = function(els, data) {
   var els_new = (isNodeList(els)) ? els : [els];
 
-  if (typeof data.nodeType !== "undefined" && data.nodeType === 1 || isNodeList(data)) {
+  if (typeof data.nodeType !== "undefined" && data.nodeType === 1) {
     for (var i = els_new.length; i--;) {
       els_new[i].insertBefore(data, els_new[i].firstChild);
     }
   } else if (typeof data === "string") {
     for (var i = els_new.length; i--;) {
       els_new[i].insertAdjacentHTML('afterbegin', data);
+    }
+  } else if (isNodeList(data)) {
+    var fragment = document.createDocumentFragment();
+    for (var i = data.length; i--;) {
+      fragment.insertBefore(data[i], fragment.firstChild);
+    }
+    for (var j = els_new.length; j--;) {
+      els_new[j].insertBefore(fragment, els_new[j].firstChild);
     }
   }
 };
