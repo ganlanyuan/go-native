980994a83533b88b7e2ec3b88cdc297eb5c4f9bf
William <ganlanyuan@gmail.com>
2016-04-04 13:19:51 -0400

    simplify priorityNav

--- tests/priority-nav/js/priorityNav.js
+++ tests/priority-nav/js/priorityNav.js
@@ -52,131 +52,139 @@
       hideAll: 0,
     }, options || {});
 
-    var fragment = document.createDocumentFragment();
+    var 
+        nav = options.nav,
+        visibleContainer = nav.querySelector('ul'),
+        visibleItems = visibleContainer.children,
+        hiddenContainer,
+        hiddenItems,
+        btn,
+        btnWidth,
+        
+        fragment = document.createDocumentFragment(),
+        bp = [],
+        bpV = [],
+        bpH = [],
+        availableSpace,
+        currentCount,
+        count,
+        initialized = false;
 
-    this.nav = options.nav;
-    this.nav.classList.add('js-priority-nav');
-    this.visibleContainer = this.nav.querySelector('ul');
-    this.visibleItems = this.visibleContainer.children;
-    this.initialized = false;
+    nav.classList.add('js-priority-nav');
 
     // init
     this.init = function () {
-      this.nav.querySelector('ul').classList.add('js-nav-visible');
-      gn.prepend(this.nav, '<button class="js-nav-toggle" data-count="">' + options.button + '</button>');
-      gn.append(this.nav, '<ul class="js-nav-hidden is-hidden"></ul>');
+      nav.querySelector('ul').classList.add('js-nav-visible');
+      gn.prepend(nav, '<button class="js-nav-toggle" data-count="">' + options.button + '</button>');
+      gn.append(nav, '<ul class="js-nav-hidden is-hidden"></ul>');
 
-      this.hiddenContainer = this.nav.querySelector('.js-nav-hidden');
-      this.hiddenItems = this.hiddenContainer.children;
-      this.btn = this.nav.querySelector('.js-nav-toggle');
-      this.btnWidth = gn.getOuterWidth(this.btn);
+      hiddenContainer = nav.querySelector('.js-nav-hidden');
+      hiddenItems = hiddenContainer.children;
+      btn = nav.querySelector('.js-nav-toggle');
+      btnWidth = gn.getOuterWidth(btn);
 
       var that = this;
-      this.btn.addEventListener('click', function () {
-        that.hiddenContainer.classList.toggle('is-hidden');
+      btn.addEventListener('click', function () {
+        hiddenContainer.classList.toggle('is-hidden');
       });
 
-      this.initialized = true;
+      initialized = true;
     };
 
     // destory
     this.destory = function () {
-      this.nav.classList.remove('js-priority-nav');
-      this.visibleContainer.classList.remove('js-nav-visible');
-      this.hiddenContainer.remove();
-      this.btn.remove();
-      this.initialized = false;
+      nav.classList.remove('js-priority-nav');
+      visibleContainer.classList.remove('js-nav-visible');
+      hiddenContainer.remove();
+      btn.remove();
+      initialized = false;
     };
     
     // get breakpoints
     this.getBreakpoints = function () {
-      this.bp = [];
-      this.bpV = [];
-      this.bpH = [];
-
-      var hideItem = false;
-      for (var i = this.visibleItems.length; i--;) {
-        if (hideItem) { this.visibleItems[i + 1].style.display = 'none'; }
-        var width = gn.getOuterWidth(this.visibleContainer) + 1;
-        this.bp.unshift(width);
-        this.bpV.unshift(width);
-        hideItem = true;
+      var isLastItem = false;
+      for (var i = visibleItems.length; i--;) {
+        if (isLastItem) { visibleItems[i + 1].style.display = 'none'; }
+        var width = gn.getOuterWidth(visibleContainer) + 1;
+        bp.unshift(width);
+        bpV.unshift(width);
+        isLastItem = true;
       }
 
-      for (var j = 0, len = this.visibleItems.length; j < len; j++) {
-        this.visibleItems[j].style.display = '';
+      for (var j = 0, len = visibleItems.length; j < len; j++) {
+        visibleItems[j].style.display = '';
       }
     };
     this.getBreakpoints();
 
     // prepend
     this.prependItemsToFragment = function () {
-      gn.prepend(fragment, this.visibleItems[this.bpV.length - 1]);
+      gn.prepend(fragment, visibleItems[bpV.length - 1]);
 
-      this.bpH.unshift(this.bpV[this.bpV.length - 1]);
-      this.bpV.splice(-1, 1);
+      bpH.unshift(bpV[bpV.length - 1]);
+      bpV.splice(-1, 1);
     };
 
     // append
     this.appendItemsToFragment = function () {
-      gn.append(fragment, this.hiddenItems[0]);
+      gn.append(fragment, hiddenItems[0]);
 
-      this.bpV.push(this.bpH[0]);
-      this.bpH.shift();
+      bpV.push(bpH[0]);
+      bpH.shift();
     };
 
     // update nav
     this.updateNav = function () {
       this.windowWidth = window.innerWidth;
-      this.outerWidth = gn.getOuterWidth(this.nav);
+      this.outerWidth = gn.getOuterWidth(nav);
 
-      if (this.outerWidth >= this.bp[this.bp.length - 1] || this.windowWidth < options.showAll) {
+      if (this.outerWidth >= bp[bp.length - 1] || this.windowWidth < options.showAll) {
 
-        if (this.initialized) {
-          if (this.bpH.length > 0) {
-            while(this.bpH.length > 0) { 
+        if (initialized) {
+          if (bpH.length > 0) {
+            while(bpH.length > 0) { 
               this.appendItemsToFragment(); 
             }
-            this.visibleContainer.appendChild(fragment);
+            visibleContainer.appendChild(fragment);
           }
 
           this.destory();
         }
 
       } else {
-        if (!this.initialized) { this.init(); }
+        if (!initialized) { this.init(); }
 
-        this.availableSpace = this.outerWidth - this.btnWidth;
+        availableSpace = this.outerWidth - btnWidth;
         if (options.hideAll === 0 || this.windowWidth >= options.hideAll) {
 
-          if (this.availableSpace <= this.bpV[this.bpV.length - 1]) {
-            while(this.availableSpace <= this.bpV[this.bpV.length - 1]) { 
+          if (availableSpace <= bpV[bpV.length - 1]) {
+            while(availableSpace <= bpV[bpV.length - 1]) { 
               this.prependItemsToFragment(); 
             }
-            this.hiddenContainer.insertBefore(fragment, this.hiddenContainer.firstChild);
+            hiddenContainer.insertBefore(fragment, hiddenContainer.firstChild);
 
           } else {
-            this.hiddenItems = this.hiddenContainer.children;
-            while(this.availableSpace > this.bpH[0]) { 
+            hiddenItems = hiddenContainer.children;
+            while(availableSpace > bpH[0]) { 
               this.appendItemsToFragment(); 
             }
-            this.visibleContainer.appendChild(fragment);
+            visibleContainer.appendChild(fragment);
           }
         } else {
-          if (this.bpH.length === this.bp.length) { return; }
+          if (bpH.length === bp.length) { return; }
 
-          while(this.bpV.length > 0) { 
+          while(bpV.length > 0) { 
             this.prependItemsToFragment(); 
           }
-          this.hiddenContainer.insertBefore(fragment, this.hiddenContainer.firstChild);
+          hiddenContainer.insertBefore(fragment, hiddenContainer.firstChild);
         }
 
         // update data-count
-        this.currentCount = this.currentCount || 0;
-        this.count = this.bpH.length || 0;
-        if (this.count !== this.currentCount) {
-          this.currentCount = this.btn.getAttribute("data-count");
-          this.btn.setAttribute("data-count", this.count);
+        currentCount = currentCount || 0;
+        count = bpH.length || 0;
+        if (count !== currentCount) {
+          currentCount = btn.getAttribute("data-count");
+          btn.setAttribute("data-count", count);
         }
       }
 
