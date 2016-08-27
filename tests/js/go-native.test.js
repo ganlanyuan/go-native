524899c39d2fe01d872505d76517fd5453f2c216
William <ganlanyuan@gmail.com>
2016-08-27 18:43:19 -0400

    add childNode.remove test

--- tests/js/go-native.test.js
+++ tests/js/go-native.test.js
@@ -60,18 +60,60 @@ function classListTest() {
       display = doc.getElementById('classList'),
       cl = element.classList,
       a = 10;
+
   if (typeof cl === 'object' &&
       cl.length !== 'undefined') {
+
     // className: 'visually-hidden classList remove toggle toggle-remove'
     cl.add('add'); // 'visually-hidden classList remove toggle toggle-remove add'
     cl.remove('remove'); // 'visually-hidden classList toggle toggle-remove add'
     cl.toggle('toggle'); // 'visually-hidden classList toggle-remove add'
     cl.toggle('toggle-remove', a > 15); // 'visually-hidden classList add'
 
+    var cn = element.className;
+
+    // test all features
     if (cl.item(0) === 'visually-hidden' &&
         cl.contains('classList') &&
-        element.className === 'visually-hidden classList add') {
+        cn === 'visually-hidden classList add') {
 
+      success(display);
+    } else {
+      fail(display);
+    }
+
+    // test individual feature
+    var clItem = doc.getElementById('classList-item'),
+        clContains = doc.getElementById('classList-contains'),
+        clAdd = doc.getElementById('classList-add'),
+        clRemove = doc.getElementById('classList-remove'),
+        clToggle = doc.getElementById('classList-toggle'),
+        clToggleForce = doc.getElementById('classList-toggle-force');
+
+    if (cl.item(0) === 'visually-hidden') { success(clItem); } else { fail(clItem); } // item
+    if (cl.contains('classList')) { success(clContains); } else { fail(clContains); } // contains
+    if (cn.indexOf('add') !== -1) { success(clAdd); } else { fail(clAdd); } // add
+    if (cn.indexOf('remove') === -1) { success(clRemove); } else { fail(clRemove); } // remove
+    if (cn.indexOf('toggle') === -1) { success(clToggle); } else { fail(clToggle); } // toggle
+    if (cn.indexOf('toggle-remove') === -1) { success(clToggleForce); } else { fail(clToggleForce); } // toggle-force
+
+  } else {
+    fail(display);
+  }
+}
+
+/*
+ * childNode.remove()
+ */
+function childNodeRemoveTest() {
+  var display = doc.getElementById('childNodeRemove'),
+      element = doc.getElementById('childNodeRemove-element'),
+      child = doc.getElementById('childNodeRemove-element-child');
+
+  if ("remove" in Element.prototype) {
+    child.remove();
+
+    if (element.children.length === 0) {
       success(display);
     } else {
       fail(display);
@@ -87,3 +129,4 @@ function classListTest() {
 
 getComputedStyleElementTest();
 classListTest();
+childNodeRemoveTest();
